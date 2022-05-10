1d48fec362b1357ad771d86359e98f2ac6fc3fe5
Carlos Sierra <carlossierra850@gmail.com>
2022-05-09 23:44:34 -0400

    deploy

--- controller/urlController.js
+++ controller/urlController.js
@@ -1,36 +1,38 @@
 const Url = require("../models/Url");
 exports.home = (req, res) => {
-    res.render('index');
-}
+  res.render("index");
+};
 exports.addUrl = async (req, res) => {
-    let response;
-    const url = new Url({
-        urlOriginal: req.body.urlOriginal
-    })
-    try{
-        let result = await url.save();
-        response = {
-            code: 201,
-            url : result.urlShort,
-            urlOriginal: req.body.urlOriginal
-        }
-    }catch(err){
-        console.log(err)
-        response = {
-            code:400,
-            err: "there was an error"
-        }
-    }
-    res.json(response);
-    next();
-}
-exports.redirectUrl = async (req,res, next) =>{
+  let response;
+  const url = new Url({
+    urlOriginal: req.body.urlOriginal,
+  });
+  try {
+    let result = await url.save();
+    response = {
+      code: 201,
+      url: result.urlShort,
+      urlOriginal: req.body.urlOriginal,
+    };
+  } catch (err) {
+    console.log(err);
+    response = {
+      code: 400,
+      err: "there was an error",
+    };
+  }
+  res.json(response);
+};
+exports.redirectUrl = async (req, res) => {
+  try {
     const url = await Url.findOne({
-        urlShort : req.params.url
+      urlShort: req.params.url,
     });
-    if(!url){
-        res.redirect("/?error=404")
+    if (!url) {
+      res.redirect("/?error=404");
     }
     res.redirect(url.urlOriginal);
-    next();
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
--- index.js
+++ index.js
@@ -5,24 +5,24 @@ const app = express();
 const mongoose = require("mongoose");
 const routes = require("./router");
 //import custom environment
-require("dotenv").config({ path:'variables.env' });
+require("dotenv").config({ path: "variables.env" });
 //read the form data
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 //conect to mongoDB
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true
-})
+  useNewUrlParser: true,
+});
 //views
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 //static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.use("/", routes());
 const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 3000;
-app.listen(port, host, () =>{
-console.log(`the server is running in ${port} at ${host} `);
+app.listen(port, host, () => {
+  console.log(`the server is running in ${port} at ${host} `);
 });
 //DEVELOPED BY CARLOS SIERRA
